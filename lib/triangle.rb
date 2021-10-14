64b467c12023cd515a34c25d935a43b8a689081d
Ambar Khare <ambar.khare24@gmail.com>
2021-10-13 21:13:20 -0700

    commit tree 9bcdbe2e43ed107b2cb7f2367e6eaa28e6b9cb1c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if x + y <= z || x + z <= y || y + z <= x || [x, y, z].any? {|a| a <= 0}
+        raise TriangleError
+    elsif x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+
+  end
+
 end
